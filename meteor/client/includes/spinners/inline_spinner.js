271769a79119cff89b393f905d4f275adbde886e
austinsand <austinsand@gmail.com>
2015-11-01 13:56:20 -0500

    Minor Spinner Fix

--- meteor/client/includes/spinners/inline_spinner.js
+++ meteor/client/includes/spinners/inline_spinner.js
@@ -1,7 +1,7 @@
 /**
  * Template Helpers
  */
-Template.inline_spinner.helpers({
+Template.InlineSpinner.helpers({
   getSize: function () {
     var scale = this.scale || 1;
     return parseInt(scale * 120);
