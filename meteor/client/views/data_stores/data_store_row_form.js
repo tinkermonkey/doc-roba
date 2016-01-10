b39fce3eaef457dd2995c548b61ae4b7b5df87e9
austinsand <austinsand@gmail.com>
2016-01-09 20:25:49 -0500

    Add auto-field-focus to DataStoreRowForm

--- meteor/client/views/data_stores/data_store_row_form.js
+++ meteor/client/views/data_stores/data_store_row_form.js
@@ -12,7 +12,11 @@ Template.DataStoreRowForm.events({});
  * Template Rendered
  */
 Template.DataStoreRowForm.rendered = function () {
+  var instance = this;
 
+  setTimeout(function () {
+    instance.$("input").get(0).focus()
+  }, 200);
 };
 
 /**
