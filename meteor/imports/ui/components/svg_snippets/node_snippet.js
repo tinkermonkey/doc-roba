b7879af2cd3fd214ab7b180e3b9e042961b8e0a4
austinsand <austinsand@gmail.com>
2016-08-24 20:57:59 -0400

    Update node snippets to have center-aligned text which uses the new wrap function

--- meteor/imports/ui/components/svg_snippets/node_snippet.js
+++ meteor/imports/ui/components/svg_snippets/node_snippet.js
@@ -4,6 +4,7 @@ import {Template} from 'meteor/templating';
 
 import {DocTreeConfig} from '../../lib/doc_tree_config.js';
 import {NodeTypes} from '../../../api/node/node_types.js';
+import {Util} from '../../../api/util.js';
 
 /**
  * Template Helpers
@@ -19,6 +20,9 @@ Template.NodeSnippet.helpers({
         return "node-page"
     }
   },
+  titleX: function () {
+    return DocTreeConfig.nodes.width / 2;
+  },
   titleY: function () {
     return DocTreeConfig.nodes.titleHeight + DocTreeConfig.nodes.borderWidth;
   }
@@ -30,15 +34,30 @@ Template.NodeSnippet.helpers({
 Template.NodeSnippet.events({});
 
 /**
+ * Template Created
+ */
+Template.NodeSnippet.onCreated(() => {
+
+});
+
+/**
  * Template Rendered
  */
-Template.NodeSnippet.rendered = function () {
+Template.NodeSnippet.onRendered(() => {
+  let self = Template.instance();
+  
+  self.autorun(() => {
+    let data = Template.currentData();
 
-};
+    d3.select("#" + self.elementId)
+        .selectAll("text.node-title")
+        .call(Util.wrapSvgText, DocTreeConfig.nodes.width - 2 * DocTreeConfig.nodes.borderWidth);
+  });
+});
 
 /**
  * Template Destroyed
  */
-Template.NodeSnippet.destroyed = function () {
+Template.NodeSnippet.onDestroyed(() => {
 
-};
+});
