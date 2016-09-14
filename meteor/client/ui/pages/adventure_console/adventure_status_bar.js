d634f8d020ae723aaa5e7715539e435e724e3f31
austinsand <austinsand@gmail.com>
2016-09-14 05:10:26 -0400

    Added feature to align adventure console accordions with the screen

--- meteor/client/ui/pages/adventure_console/adventure_status_bar.js
+++ meteor/client/ui/pages/adventure_console/adventure_status_bar.js
@@ -6,6 +6,9 @@ import { Template } from 'meteor/templating';
  * Template Helpers
  */
 Template.AdventureStatusBar.helpers({
+  getHeaderMargin(){
+    return Template.instance().headerMargin.get();
+  },
   adventureStatus(){
     return this.adventure.get().status
   }
@@ -20,14 +23,25 @@ Template.AdventureStatusBar.events({});
  * Template Created
  */
 Template.AdventureStatusBar.onCreated(() => {
-  
+  let instance          = Template.instance();
+  instance.headerMargin = new ReactiveVar();
 });
 
 /**
  * Template Rendered
  */
 Template.AdventureStatusBar.onRendered(() => {
-  
+  let instance = Template.instance();
+  instance.autorun(() => {
+    let viewport = instance.data.viewport.get(),
+        header = instance.$(".adventure-status-header"),
+        offset = $(".roba-accordion-content").first().offset(),
+        currentMargin = parseInt(header.css("margin-top"));
+    
+    if(currentMargin >= 0 && viewport && viewport.offset){
+      instance.headerMargin.set(currentMargin + viewport.offset.top - offset.top);
+    }
+  })
 });
 
 /**
