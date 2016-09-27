a57440aa0b27ccfd17e930830fd834815f329bdd
austinsand <austinsand@gmail.com>
2016-09-27 07:47:10 -0400

    Added NodeCheckEditList to the page_view_edit_panel

--- meteor/client/ui/components/edit_panels/page_view_edit_panel.js
+++ meteor/client/ui/components/edit_panels/page_view_edit_panel.js
@@ -1,10 +1,10 @@
 import './page_view_edit_panel.html';
-
-import {Template} from 'meteor/templating';
-
+import { Template } from 'meteor/templating';
+import { NodeCheckTypes } from '../../../../imports/api/node/node_check_types.js';
 import '../editable_fields/editable_node_type.js';
 import '../editable_fields/editable_code/editable_code.js';
 import '../editable_fields/editable_nav_menu_list.js';
+import '../node_checks/node_check_edit_list.js';
 
 /**
  * Template Helpers
@@ -12,9 +12,12 @@ import '../editable_fields/editable_nav_menu_list.js';
 Template.PageViewEditPanel.helpers({
   editParamsPanel(){
     let platformType = this.platformType();
-    if(platformType){
+    if (platformType) {
       return platformType.nodeEditParamsTemplate();
     }
+  },
+  checkTypes(){
+    return NodeCheckTypes;
   }
 });
 
