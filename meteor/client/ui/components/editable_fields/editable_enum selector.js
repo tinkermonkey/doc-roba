add3a99b26a8520df1df940580e23c40ba4b98ba
austinsand <austinsand@gmail.com>
2016-09-27 07:45:40 -0400

    Fixed editable enum selector to work with complex lookups

--- meteor/client/ui/components/editable_fields/editable_enum selector.js
+++ meteor/client/ui/components/editable_fields/editable_enum selector.js
@@ -1,9 +1,7 @@
 import './editable_enum_selector.html';
-
-import {Template} from 'meteor/templating';
-import {RobaDialog} from 'meteor/austinsand:roba-dialog';
-
-import {Util} from '../../../../imports/api/util.js';
+import { Template } from 'meteor/templating';
+import { RobaDialog } from 'meteor/austinsand:roba-dialog';
+import { Util } from '../../../../imports/api/util.js';
 
 /**
  * Template Helpers
@@ -11,8 +9,9 @@ import {Util} from '../../../../imports/api/util.js';
 Template.EditableEnumSelector.helpers({
   renderValue(){
     let lookup = Template.instance().lookup;
-    if(lookup && this.value != undefined){
-      return Util.camelToTitle(lookup[this.value])
+    if (lookup && this.value != undefined) {
+      //console.log("EditableEnumSelector.renderValue: ", this.value, this);
+      return Util.camelToTitle(lookup[ this.value ])
     }
   }
 });
@@ -27,7 +26,18 @@ Template.EditableEnumSelector.events({});
  */
 Template.EditableEnumSelector.onCreated(() => {
   let instance = Template.instance();
-  instance.lookup = _.invert(instance.data.enum);
+  
+  // translate the value key to the display key
+  if (instance.data.displayKey) {
+    instance.lookup = {};
+    _.keys(instance.data.enum).forEach((key) => {
+      let displayKey = instance.data.enum[key][instance.data.displayKey];
+      instance.lookup[key] = displayKey;
+    });
+  } else {
+    instance.lookup = _.invert(instance.data.enum);
+  }
+  //console.log("EditableEnumSelector.lookup:", instance.lookup);
 });
 
 /**
@@ -37,21 +47,22 @@ Template.EditableEnumSelector.onRendered(() => {
   let instance = Template.instance();
   
   instance.$('.editable-enum-selector').editable({
-    mode: instance.data.mode || "inline",
-    type: "select",
-    source: _.map(_.keys(instance.lookup), function (key) {
-      return {value: key, text: Util.camelToTitle(instance.lookup[key])};
+    mode     : instance.data.mode || "inline",
+    type     : "select",
+    source   : _.map(_.keys(instance.lookup), function (key) {
+      return { value: key, text: Util.camelToTitle(instance.lookup[ key ]) };
     }),
     highlight: false,
     display() {
     },
     success(response, newValue) {
-      var editedElement = this,
-          lookup = instance.lookup.get();
-      if (lookup[newValue]) {
-        $(editedElement).trigger("edited", [newValue]);
+      var editedElement = this;
+      
+      if (instance.lookup[ newValue ]) {
+        $(editedElement).trigger("edited", [ newValue ]);
       } else {
-        RobaDialog.error("Variable type update failed. Variable type not known: " + newValue);
+        RobaDialog.error("EditableEnumSelector change failed. Data key not known: " + newValue);
+        console.log("EditableEnumSelector valid values:", instance.lookup, newValue);
       }
       
       setTimeout(function () {
