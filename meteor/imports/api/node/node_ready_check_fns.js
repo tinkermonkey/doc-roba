481bb34dc959b0a3668bef55b5d162c6b525debd
austinsand <austinsand@gmail.com>
2016-09-28 06:43:31 -0400

    Removed un-used and high-maintenance "type" fields from CheckFn enums

--- meteor/imports/api/node/node_ready_check_fns.js
+++ meteor/imports/api/node/node_ready_check_fns.js
@@ -1,42 +1,35 @@
 export const NodeReadyCheckFns = {
   waitForChecked : {
-    type : 0,
     label: "Is Checked",
     fn   : "waitForChecked",
     args : []
   },
   waitForEnabled : {
-    type : 1,
     label: "Is Enabled",
     fn   : "waitForEnabled",
     args : []
   },
   waitForExist  : {
-    type : 2,
     label: "Exists",
     fn   : "waitForExist",
     args : []
   },
   waitForSelected: {
-    type : 3,
     label: "Is Selected",
     fn   : "waitForSelected",
     args : []
   },
   waitForText    : {
-    type : 4,
     label: "Has Text",
     fn   : "waitForText",
     args : [ "text" ]
   },
   waitForValue   : {
-    type : 5,
     label: "Has Value",
     fn   : "waitForValue",
     args : [ "value" ]
   },
   waitForVisible : {
-    type : 6,
     label: "Is Visible",
     fn   : "waitForVisible",
     args : []
--- meteor/imports/api/node/node_valid_check_fns.js
+++ meteor/imports/api/node/node_valid_check_fns.js
@@ -1,87 +1,81 @@
 export const NodeValidCheckFns = {
   isVisible: {
-    type : 0,
     label: "Is Visible",
     fn   : "isVisible",
     args : []
   },
   isEnabled: {
-    type : 1,
     label: "Is Enabled",
     fn   : "isEnabled",
     args : []
   },
   isExisting: {
-    type : 2,
     label: "Is Existing",
     fn   : "isExisting",
     args : []
   },
   isSelected: {
-    type : 3,
     label: "Is Selected",
     fn   : "isSelected",
     args : []
   },
   isChecked: {
-    type : 4,
     label: "Is Checked",
     fn   : "isChecked",
     args : []
   },
   isNotVisible: {
-    type : 5,
     label: "Is Not Visible",
     fn   : "isNotVisible",
     args : []
   },
   isNotEnabled: {
-    type : 6,
     label: "Is Not Enabled",
     fn   : "isNotEnabled",
     args : []
   },
   isNotExisting: {
-    type : 7,
     label: "Is Not Existing",
     fn   : "isNotExisting",
     args : []
   },
   isNotSelected: {
-    type : 8,
     label: "Is Not Selected",
     fn   : "isNotSelected",
     args : []
   },
   isNotChecked: {
-    type : 9,
     label: "Is Not Checked",
     fn   : "isNotChecked",
     args : []
   },
   hasText: {
-    type : 10,
     label: "Has Text",
     fn   : "hasText",
-    args : []
+    args : [
+      { label: "Text" }
+    ]
   },
   hasValue: {
-    type : 11,
     label: "Has Value",
     fn   : "hasValue",
-    args : []
+    args : [
+      { label: "Value" }
+    ]
   },
   doesNotHaveText: {
-    type : 12,
     label: "Does Not Have Text",
     fn   : "doesNotHaveText",
-    args : []
+    args : [
+      { label: "Text" }
+    ]
   },
   doesNotHaveValue: {
-    type : 13,
     label: "Does Not Have Value",
     fn   : "doesNotHaveValue",
-    args : []
+    args : [
+      { label: "Value" }
+    ]
   }
 };
 /*
