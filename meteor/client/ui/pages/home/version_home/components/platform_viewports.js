d60c4be0163a1d687f072189d38827961498ded4
austinsand <austinsand@gmail.com>
2016-10-02 15:23:28 -0400

    Added default flag for PlatformViewport, updated AspectRatio maintenance code

--- meteor/client/ui/pages/home/version_home/components/platform_viewports.js
+++ meteor/client/ui/pages/home/version_home/components/platform_viewports.js
@@ -1,6 +1,8 @@
 import './platform_viewports.html';
-
 import { Template } from 'meteor/templating';
+import { RobaDialog } from 'meteor/austinsand:roba-dialog';
+import { PlatformViewports } from '../../../../../../imports/api/platform_configuration/platform_viewport.js';
+import '../../../../components/editable_fields/editable_radio_button.js';
 
 /**
  * Template Helpers
@@ -10,13 +12,38 @@ Template.PlatformViewports.helpers({});
 /**
  * Template Event Handlers
  */
-Template.PlatformViewports.events({});
+Template.PlatformViewports.events({
+  "edited .editable"(e, instance, newValue){
+    let target             = $(e.target),
+        dataKey            = target.attr("data-key"),
+        viewportId         = target.closest(".sortable-table-row").attr("data-pk"),
+        update             = { $set: {} };
+    update.$set[ dataKey ] = newValue;
+    PlatformViewports.update({ _id: viewportId }, update, (error) => {
+      if (error) {
+        RobaDialog.error("Updating Viewport failed: " + error);
+      }
+    });
+  },
+  "click .btn-delete"(e, instance){
+    let osId = $(e.target).closest(".sortable-table-row").attr("data-pk");
+    RobaDialog.ask("Delete Viewport?", "Are you sure that you want to delete this viewport record from this platform?", () => {
+          PlatformViewports.remove(osId, function (error, response) {
+            RobaDialog.hide();
+            if (error) {
+              RobaDialog.error("Delete failed: " + error.message);
+            }
+          });
+        }
+    );
+  }
+});
 
 /**
  * Template Created
  */
 Template.PlatformViewports.onCreated(() => {
-
+  
 });
 
 /**
--- meteor/imports/api/platform_configuration/platform_viewport.js
+++ meteor/imports/api/platform_configuration/platform_viewport.js
@@ -37,16 +37,13 @@ export const PlatformViewport = new SimpleSchema({
     defaultValue: 768
   },
   aspectRatio     : {
-    type: Number,
-    decimal: true,
-    autoValue() {
-      let width  = this.field('width'),
-          height = this.field('height');
-      console.log("aspectRatio.autoValue:", width, height, width.value / height.value);
-      if(width.isSet && height.isSet){
-        return width.value / height.value;
-      }
-    }
+    type   : Number,
+    decimal: true
+  },
+  // Mark the default viewport for a platform
+  default: {
+    type: Boolean,
+    defaultValue: false
   },
   // Standard tracking fields
   dateCreated     : {
@@ -74,6 +71,39 @@ PlatformViewports.attachSchema(PlatformViewport);
 PlatformViewports.deny(Auth.ruleSets.deny.ifNotTester);
 PlatformViewports.allow(Auth.ruleSets.allow.ifAuthenticated);
 ChangeTracker.TrackChanges(PlatformViewports, "platform_viewports");
+
+// Listed for changes and update the aspect ratio
+if (Meteor.isServer) {
+  // Maintain the aspect ratio
+  PlatformViewports.before.insert(function (userId, doc) {
+    doc.aspectRatio = doc.width / doc.height;
+  });
+  PlatformViewports.before.update(function (userId, doc, changedFields, modifier) {
+    modifier.$set = modifier.$set || {};
+    if (_.contains(changedFields, 'width') || _.contains(changedFields, 'height')) {
+      let width  = modifier.$set.width || doc.width,
+          height = modifier.$set.height || doc.height;
+      console.log("PlatformViewports.before.update setting aspectRatio:", width / height);
+      modifier.$set.aspectRatio = width / height;
+    }
+  });
+  
+  // Maintain the default flag
+  PlatformViewports.after.update(function (userId, doc, changedFields, modifier) {
+    // If the default flag is set to true
+    if (_.contains(changedFields, 'default') && doc.default == true) {
+      // Set the other viewports for this platform to default = false
+      PlatformViewports.find({
+        _id: { $ne: doc._id },
+        platformId: doc.platformId,
+        default: true
+      }).forEach((viewport) => {
+        console.log("Setting default from true to false:", viewport);
+        PlatformViewports.update({_id: viewport._id}, {$set: {default: false}});
+      });
+    }
+  });
+}
 
 /**
  * Helpers
