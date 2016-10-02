ac5ab4b4f086d8af329e4152430eca789c6a4de7
austinsand <austinsand@gmail.com>
2016-10-02 15:20:47 -0400

    Removed EditableFieldYesNo and replaced usages with EditableCheckbox

--- meteor/client/ui/pages/home/version_home/components/version_server_list.js
+++ meteor/client/ui/pages/home/version_home/components/version_server_list.js
@@ -1,22 +1,17 @@
 import './version_server_list.html';
-
-import {Template} from 'meteor/templating';
-import {RobaDialog} from 'meteor/austinsand:roba-dialog';
-import {EditableTextField} from 'meteor/austinsand:editable-text-field';
-
-import {ProjectVersions} from '../../../../../../imports/api/project/project_version.js';
-import {TestServers} from '../../../../../../imports/api/test_server/test_server.js';
-import {Datastores} from '../../../../../../imports/api/datastore/datastore.js';
-
+import { Template } from 'meteor/templating';
+import { RobaDialog } from 'meteor/austinsand:roba-dialog';
+import { EditableTextField } from 'meteor/austinsand:editable-text-field';
+import { TestServers } from '../../../../../../imports/api/test_server/test_server.js';
 import '../../../../components/editable_fields/editable_autoform/editable_autoform.js';
-import '../../../../components/editable_fields/editable_field_yes_no.js';
+import '../../../../components/editable_fields/editable_checkbox.js';
 
 /**
  * Template Helpers
  */
 Template.VersionServerList.helpers({
   sortedServers() {
-    return TestServers.find({projectVersionId: this._id}, {sort: {order: 1}}).fetch();
+    return TestServers.find({ projectVersionId: this._id }, { sort: { order: 1 } }).fetch();
   },
   configSchema() {
     let projectVersion = Template.parentData(1);
@@ -29,36 +24,36 @@ Template.VersionServerList.helpers({
  */
 Template.VersionServerList.events({
   "click .btn-add-server"() {
-    var instance = Template.instance(),
+    var instance       = Template.instance(),
         projectVersion = this,
-        order = instance.$(".server-list-row").length;
+        order          = instance.$(".server-list-row").length;
     
     TestServers.insert({
-      projectId: projectVersion.projectId,
+      projectId       : projectVersion.projectId,
       projectVersionId: projectVersion._id,
-      title: "New Server",
-      url: "HTTP://server.com",
-      active: true,
-      order: order
+      title           : "New Server",
+      url             : "HTTP://server.com",
+      active          : true,
+      order           : order
     });
   },
   "click .server-list-row .btn-delete"() {
     var server = this;
-
+    
     RobaDialog.show({
-      title: "Delete Server?",
-      text: "Are you sure that you want to delete the server configuration <span class='label label-primary'>" + server.title + "</span> from this version?",
-      width: 400,
+      title  : "Delete Server?",
+      text   : "Are you sure that you want to delete the server configuration <span class='label label-primary'>" + server.title + "</span> from this version?",
+      width  : 400,
       buttons: [
-        {text: "Cancel"},
-        {text: "Delete"}
+        { text: "Cancel" },
+        { text: "Delete" }
       ],
       callback(btn) {
         //console.log("Dialog button pressed: ", btn);
-        if(btn == "Delete"){
+        if (btn == "Delete") {
           TestServers.remove(server._id, function (error, response) {
             RobaDialog.hide();
-            if(error){
+            if (error) {
               console.error("Delete failed: " + error.message);
               RobaDialog.error("Delete failed" + error.message);
             }
@@ -72,12 +67,12 @@ Template.VersionServerList.events({
   "edited .editable"(e, instance, newValue) {
     e.stopImmediatePropagation();
     
-    var serverId = $(e.target).closest(".server-list-row").attr("data-pk"),
-      dataKey = $(e.target).attr("data-key"),
-      update = {$set: {}};
-    update["$set"][dataKey] = newValue;
+    var serverId                = $(e.target).closest(".server-list-row").attr("data-pk"),
+        dataKey                 = $(e.target).attr("data-key"),
+        update                  = { $set: {} };
+    update[ "$set" ][ dataKey ] = newValue;
     TestServers.update(serverId, update, function (error, response) {
-      if(error){
+      if (error) {
         console.error("Server update failed: " + error.message);
         RobaDialog.error("Server update failed: " + error.message);
       }
@@ -89,7 +84,7 @@ Template.VersionServerList.events({
  * Template Created
  */
 Template.VersionServerList.created = function () {
-  let instance = Template.instance();
+  let instance          = Template.instance();
   instance.configSchema = new ReactiveVar({});
 };
 
@@ -98,78 +93,78 @@ Template.VersionServerList.created = function () {
  */
 Template.VersionServerList.rendered = function () {
   var instance = Template.instance();
-
+  
   // Server list is sortable (manually)
   instance.$(".sortable-table")
-    .sortable({
-      items: "> .sortable-table-row",
-      handle: ".drag-handle",
-      helper(e, ui) {
-        // fix the width
-        ui.children().each(function() {
-          $(this).width($(this).width());
-        });
-        return ui;
-      },
-      axis: "y",
-      forcePlaceholderSize: true,
-      update(event, ui) {
-        var order;
-        instance.$(".server-list-row").each(function (i, el) {
-          order = $(el).attr("data-sort-order");
-          if(order != i){
-            console.log("Updating order: ", i, $(el).attr("data-pk"));
-            TestServers.update($(el).attr("data-pk"), {$set: {order: i}}, function (error, response) {
-              if(error){
-                RobaDialog.error("Server order update failed: " + error.message);
-              }
-            });
-          }
-        });
-      }
-    })
-    .disableSelection();
-
+      .sortable({
+        items               : "> .sortable-table-row",
+        handle              : ".drag-handle",
+        helper(e, ui) {
+          // fix the width
+          ui.children().each(function () {
+            $(this).width($(this).width());
+          });
+          return ui;
+        },
+        axis                : "y",
+        forcePlaceholderSize: true,
+        update(event, ui) {
+          var order;
+          instance.$(".server-list-row").each(function (i, el) {
+            order = $(el).attr("data-sort-order");
+            if (order != i) {
+              console.log("Updating order: ", i, $(el).attr("data-pk"));
+              TestServers.update($(el).attr("data-pk"), { $set: { order: i } }, function (error, response) {
+                if (error) {
+                  RobaDialog.error("Server order update failed: " + error.message);
+                }
+              });
+            }
+          });
+        }
+      })
+      .disableSelection();
+  
   // Make the field list sortable
   /*
-  instance.autorun(function () {
-    //var servers = TestServers.find({projectVersionId: instance.data.version._id});
-    //instance.$(".sortable-table").sortable("refresh");
-  });
-
-  // Setup the config schema initial value
-  instance.autorun(() => {
-    console.log("VersionServerList.autorun - updating DatastoreSchemas");
-    let projectVersionId = FlowRouter.getParam("projectVersionId"),
-        projectVersion = ProjectVersions.findOne({_id: projectVersionId}),
-        datastore = projectVersion.serverConfigDatastore();
-    
-    console.log("projectVersionId:", projectVersionId);
-    console.log("projectVersion:", projectVersion);
-    console.log("datastore:", datastore);
-    DatastoreSchemas[datastore._id] = DSUtil.simpleSchema(datastore.schema);
-    instance.configSchema.set(DatastoreSchemas[datastore._id]);
-  });
-  */
+   instance.autorun(function () {
+   //var servers = TestServers.find({projectVersionId: instance.data.version._id});
+   //instance.$(".sortable-table").sortable("refresh");
+   });
+   
+   // Setup the config schema initial value
+   instance.autorun(() => {
+   console.log("VersionServerList.autorun - updating DatastoreSchemas");
+   let projectVersionId = FlowRouter.getParam("projectVersionId"),
+   projectVersion = ProjectVersions.findOne({_id: projectVersionId}),
+   datastore = projectVersion.serverConfigDatastore();
+   
+   console.log("projectVersionId:", projectVersionId);
+   console.log("projectVersion:", projectVersion);
+   console.log("datastore:", datastore);
+   DatastoreSchemas[datastore._id] = DSUtil.simpleSchema(datastore.schema);
+   instance.configSchema.set(DatastoreSchemas[datastore._id]);
+   });
+   */
   /*
-  var ds = Datastores.findOne({dataKey: "server_config_" + instance.data._id});
-  if(ds.schema){
-    DatastoreSchemas[ds._id] = DSUtil.simpleSchema(ds.schema);
-    instance.configSchema.set(DatastoreSchemas[ds._id]);
-  }
-  */
-
+   var ds = Datastores.findOne({dataKey: "server_config_" + instance.data._id});
+   if(ds.schema){
+   DatastoreSchemas[ds._id] = DSUtil.simpleSchema(ds.schema);
+   instance.configSchema.set(DatastoreSchemas[ds._id]);
+   }
+   */
+  
   // Keep the server config simple schema up to date
   /*
-  instance.configObservation = Datastores.find({dataKey: "server_config_" + instance.data._id}).observeChanges({
-    changed(id, fields) {
-      if(_.contains(fields, "schema")){
-        DatastoreSchemas[id] = DSUtil.simpleSchema(fields.schema);
-        instance.configSchema.set(DatastoreSchemas[id]);
-      }
-    }
-  });
-  */
+   instance.configObservation = Datastores.find({dataKey: "server_config_" + instance.data._id}).observeChanges({
+   changed(id, fields) {
+   if(_.contains(fields, "schema")){
+   DatastoreSchemas[id] = DSUtil.simpleSchema(fields.schema);
+   instance.configSchema.set(DatastoreSchemas[id]);
+   }
+   }
+   });
+   */
 };
 
 /**
--- meteor/client/ui/pages/home/version_home/components/version_test_system_list.js
+++ meteor/client/ui/pages/home/version_home/components/version_test_system_list.js
@@ -1,13 +1,10 @@
 import './version_test_system_list.html';
-
-import {Template} from 'meteor/templating';
-import {RobaDialog} from 'meteor/austinsand:roba-dialog';
-import {EditableTextField} from 'meteor/austinsand:editable-text-field';
-
-import {TestSystems} from '../../../../../../imports/api/test_system/test_system.js';
-import {TestAgentTypes} from '../../../../../../imports/api/test_agent/test_agent_types.js';
-
-import '../../../../components/editable_fields/editable_field_yes_no.js';
+import { Template } from 'meteor/templating';
+import { RobaDialog } from 'meteor/austinsand:roba-dialog';
+import { EditableTextField } from 'meteor/austinsand:editable-text-field';
+import { TestSystems } from '../../../../../../imports/api/test_system/test_system.js';
+import { TestAgentTypes } from '../../../../../../imports/api/test_agent/test_agent_types.js';
+import '../../../../components/editable_fields/editable_checkbox.js';
 import '../../../../components/editable_fields/editable_test_agent_type.js';
 import '../../../../components/editable_fields/editable_test_agent_list.js';
 
@@ -16,7 +13,7 @@ import '../../../../components/editable_fields/editable_test_agent_list.js';
  */
 Template.VersionTestSystemList.helpers({
   sortedTestSystems() {
-    return TestSystems.find({projectVersionId: this._id}, {sort: {order: 1}}).fetch();
+    return TestSystems.find({ projectVersionId: this._id }, { sort: { order: 1 } }).fetch();
   }
 });
 
@@ -25,20 +22,20 @@ Template.VersionTestSystemList.helpers({
  */
 Template.VersionTestSystemList.events({
   "click .btn-add-test-system"(e, instance) {
-    var order = instance.$(".sortable-table-row").length,
+    var order          = instance.$(".sortable-table-row").length,
         projectVersion = this;
-
+    
     TestSystems.insert({
-      projectId: projectVersion.projectId,
+      projectId       : projectVersion.projectId,
       projectVersionId: projectVersion._id,
-      title: "New Test System",
-      type: TestAgentTypes.selenium,
-      hostname: "test.test.com",
-      port: "4444",
-      active: true,
-      order: order
+      title           : "New Test System",
+      type            : TestAgentTypes.selenium,
+      hostname        : "test.test.com",
+      port            : "4444",
+      active          : true,
+      order           : order
     }, function (error, response) {
-      if(error){
+      if (error) {
         RobaDialog.error("Insert failed: " + error.message);
       } else {
         setTimeout(function () {
@@ -49,21 +46,21 @@ Template.VersionTestSystemList.events({
   },
   "click .sortable-table-row .btn-delete"(e, instance) {
     var testSystem = this;
-
+    
     RobaDialog.show({
-      title: "Delete Test System Configuration?",
-      text: "Are you sure that you want to delete the test system configuration <span class='label label-primary'>" + testSystem.title + "</span> from this version?",
-      width: 400,
+      title  : "Delete Test System Configuration?",
+      text   : "Are you sure that you want to delete the test system configuration <span class='label label-primary'>" + testSystem.title + "</span> from this version?",
+      width  : 400,
       buttons: [
-        {text: "Cancel"},
-        {text: "Delete"}
+        { text: "Cancel" },
+        { text: "Delete" }
       ],
       callback(btn) {
         //console.log("Dialog button pressed: ", btn);
-        if(btn == "Delete"){
+        if (btn == "Delete") {
           TestSystems.remove(testSystem._id, function (error, response) {
             RobaDialog.hide();
-            if(error){
+            if (error) {
               RobaDialog.error("Delete failed: " + error.message);
             }
           });
@@ -75,12 +72,12 @@ Template.VersionTestSystemList.events({
   },
   "edited .editable"(e, instance, newValue) {
     e.stopImmediatePropagation();
-    var testSystemId = $(e.target).closest(".sortable-table-row").attr("data-pk"),
-      dataKey = $(e.target).attr("data-key"),
-      update = {$set: {}};
-    update["$set"][dataKey] = newValue;
+    var testSystemId            = $(e.target).closest(".sortable-table-row").attr("data-pk"),
+        dataKey                 = $(e.target).attr("data-key"),
+        update                  = { $set: {} };
+    update[ "$set" ][ dataKey ] = newValue;
     TestSystems.update(testSystemId, update, function (error) {
-      if(error){
+      if (error) {
         RobaDialog.error("Test System update failed: " + error.message);
       }
     });
@@ -92,41 +89,41 @@ Template.VersionTestSystemList.events({
  */
 Template.VersionTestSystemList.rendered = function () {
   var instance = Template.instance();
-
+  
   instance.$(".sortable-table")
-    .sortable({
-      items: "> .sortable-table-row",
-      handle: ".drag-handle",
-      helper(e, ui) {
-        // fix the width
-        ui.children().each(function() {
-          $(this).width($(this).width());
-        });
-        return ui;
-      },
-      axis: "y",
-      forcePlaceholderSize: true,
-      update(event, ui) {
-        var order;
-        instance.$(".sortable-table-row").each(function (i, el) {
-          order = $(el).attr("data-sort-order");
-          if(order != i){
-            console.log("Updating order: ", i, $(el).attr("data-pk"));
-            TestSystems.update($(el).attr("data-pk"), {$set: {order: i}}, function (error, response) {
-              if(error){
-                RobaDialog.error("Test System order update failed: " + error.message);
-              }
-            });
-          }
-        });
-      }
-    })
-    .disableSelection();
+      .sortable({
+        items               : "> .sortable-table-row",
+        handle              : ".drag-handle",
+        helper(e, ui) {
+          // fix the width
+          ui.children().each(function () {
+            $(this).width($(this).width());
+          });
+          return ui;
+        },
+        axis                : "y",
+        forcePlaceholderSize: true,
+        update(event, ui) {
+          var order;
+          instance.$(".sortable-table-row").each(function (i, el) {
+            order = $(el).attr("data-sort-order");
+            if (order != i) {
+              console.log("Updating order: ", i, $(el).attr("data-pk"));
+              TestSystems.update($(el).attr("data-pk"), { $set: { order: i } }, function (error, response) {
+                if (error) {
+                  RobaDialog.error("Test System order update failed: " + error.message);
+                }
+              });
+            }
+          });
+        }
+      })
+      .disableSelection();
 };
 
 /**
  * Template Destroyed
  */
 Template.VersionTestSystemList.destroyed = function () {
-
+  
 };
