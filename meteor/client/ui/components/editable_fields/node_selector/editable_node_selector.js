c741d073394eabbc5d69cff5f6c0749e0a50bf4f
austinsand <austinsand@gmail.com>
2016-10-02 15:21:07 -0400

    Code Cleanup

--- meteor/client/ui/components/editable_fields/node_selector/editable_node_selector.js
+++ meteor/client/ui/components/editable_fields/node_selector/editable_node_selector.js
@@ -1,8 +1,6 @@
 import './editable_node_selector.html';
-
-import {Blaze} from 'meteor/blaze';
-import {Template} from 'meteor/templating';
-
+import { Blaze } from 'meteor/blaze';
+import { Template } from 'meteor/templating';
 import './x_editable_node_selector.js';
 import './x_editable_node_selector.css';
 
@@ -28,35 +26,36 @@ Template.EditableNodeSelector.created = function () {
  */
 Template.EditableNodeSelector.rendered = function () {
   var instance = Template.instance();
-
+  
   instance.$(".editable").editable({
-    type: "nodeSelector",
-    mode: instance.data.mode || "popup",
-    placement: instance.data.placement || "auto",
-    value: instance.data.value,
+    type            : "nodeSelector",
+    mode            : instance.data.mode || "popup",
+    placement       : instance.data.placement || "auto",
+    value           : instance.data.value,
     projectVersionId: instance.data.projectVersionId,
-    parentInstance: instance,
-    highlight: false,
-    display() {},
+    parentInstance  : instance,
+    highlight       : false,
+    display() {
+    },
     success(response, newValue) {
       console.log("EditableNodeSelector edited: ", newValue);
       var editedElement = this;
-      $(editedElement).trigger("edited", [newValue]);
+      $(editedElement).trigger("edited", [ newValue ]);
       setTimeout(function () {
         $(editedElement).removeClass('editable-unsaved');
       }, 10);
     }
   });
-
+  
   // this event listener needs to be registered directly
-  instance.$(".editable").on("hidden", function() {
-    if(instance.searchView){
+  instance.$(".editable").on("hidden", function () {
+    if (instance.searchView) {
       setTimeout(function () {
         Blaze.remove(instance.searchView);
       }, 100);
     }
   });
-
+  
   instance.autorun(function () {
     var data = Template.currentData();
     instance.$(".editable").editable("setValue", data.value);
