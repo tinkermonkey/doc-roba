fa6e2538c12b05eb9122a03d229fc1c3eceaef2b
austinsand <austinsand@gmail.com>
2017-03-09 06:58:19 -0500

    Moved test result launch code from the collection to the server
    This didn’t work at the collection level because it spans client and
    server and the dependency checks would fail.

--- meteor/imports/api/test_result/server/methods.js
+++ meteor/imports/api/test_result/server/methods.js
@@ -1,19 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
-
+import { Accounts } from 'meteor/accounts-base';
 // Collections
-import {TestResults} from '../test_result.js';
-import {TestResultRoles} from '../test_result_role.js';
-import {TestResultSteps} from '../test_result_step.js';
-import {TestAgents} from '../../test_agent/test_agent.js';
-import {TestSystems} from '../../test_system/test_system.js';
-import {TestServers} from '../../test_server/test_server.js';
-
+import { LogMessages } from '../../log_message/log_message.js';
+import { Nodes } from '../../nodes/nodes.js';
+import { PlatformViewports } from '../../platform_configuration/platform_viewport.js';
+import { Screenshots } from '../../screenshot/screenshot.js';
+import { TestResults } from '../test_result.js';
+import { TestResultRoles } from '../test_result_role.js';
+import { TestResultSteps } from '../test_result_step.js';
+import { TestAgents } from '../../test_agent/test_agent.js';
+import { TestSystems } from '../../test_system/test_system.js';
+import { TestServers } from '../../test_server/test_server.js';
 // Enums
-import {TestResultStatus} from '../test_result_status.js';
-import {TestResultCodes} from '../test_result_codes.js';
-import {TestCaseStepTypes} from '../../test_case/test_case_step_types.js';
-import {ScreenshotKeys} from '../../screenshot/screenshot_keys.js';
+import { TestResultStatus } from '../test_result_status.js';
+import { TestResultCodes } from '../test_result_codes.js';
+import { TestCaseStepTypes } from '../../test_case/test_case_step_types.js';
+import { ScreenshotKeys } from '../../screenshot/screenshot_keys.js';
+import { ProcessLauncher } from '../../process_launcher/process_launcher.js';
 
 Meteor.methods({
   /**
@@ -21,12 +25,12 @@ Meteor.methods({
    * @returns Object
    */
   loadTestEnums() {
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     
     return {
-      resultStatus: TestResultStatus,
-      resultCodes: TestResultCodes,
-      stepTypes: TestCaseStepTypes,
+      resultStatus  : TestResultStatus,
+      resultCodes   : TestResultCodes,
+      stepTypes     : TestCaseStepTypes,
       screenshotKeys: ScreenshotKeys
     };
   },
@@ -37,27 +41,53 @@ Meteor.methods({
    */
   loadTestRoleManifest(testResultRoleId) {
     console.debug("loadTestRoleManifest: ", testResultRoleId);
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     check(testResultRoleId, String);
     
     // Require authentication
-    if(!this.userId){
+    if (!this.userId) {
       throw new Meteor.Error("Authentication Failed", "User is not authenticated");
     }
     
     // load the role and the steps
     var result = {
-      role: TestResultRoles.findOne(testResultRoleId),
-      steps: TestResultSteps.find({testResultRoleId: testResultRoleId}, {sort: {order: 1}}).fetch()
+      role : TestResultRoles.findOne(testResultRoleId),
+      steps: TestResultSteps.find({ testResultRoleId: testResultRoleId }, { sort: { order: 1 } }).fetch()
     };
     
     // load the result
     result.result = TestResults.findOne(result.role.testResultId);
     
     // load the test system, test agent, and server
-    result.system = TestSystems.findOne({staticId: result.role.testSystemId, projectVersionId: result.role.projectVersionId});
-    result.agent  = TestAgents.findOne({staticId: result.role.testAgentId, projectVersionId: result.role.projectVersionId});
-    result.server = TestServers.findOne({staticId: result.result.serverId, projectVersionId: result.role.projectVersionId});
+    result.system   = TestSystems.findOne({
+      staticId        : result.role.testSystemId,
+      projectVersionId: result.role.projectVersionId
+    });
+    result.agent    = TestAgents.findOne({
+      staticId        : result.role.testAgentId,
+      projectVersionId: result.role.projectVersionId
+    });
+    result.server   = TestServers.findOne({
+      staticId        : result.result.serverId,
+      projectVersionId: result.role.projectVersionId
+    });
+    
+    if(result.steps && result.steps.length){
+      var nodeFound = false,
+          i = 0;
+      while(!nodeFound && i < result.steps.length){
+        var step = result.steps[0];
+        if(step.node){
+          //
+          var node = Nodes.findOne(step.node._id);
+          if(node){
+            var platformConfig = node.platformConfig();
+            result.viewport = PlatformViewports.findOne({platformId: platformConfig._id, default: true});
+          }
+        }
+        i++;
+      }
+    }
     
     return result;
   },
@@ -69,43 +99,45 @@ Meteor.methods({
    * @param result
    */
   testRoleFailed(testResultRoleId, result) {
-    console.info("testRoleFailed: " + testResultRoleId + ", " + result);
-    check(Meteor.user(), Object);
+    console.info("testRoleFailed: ", testResultRoleId, ", ", result);
+    check(Meteor.userId(), String);
     check(testResultRoleId, String);
     
-    var testResultRole = TestResultRoles.findOne({_id: testResultRoleId});
-    if(testResultRole && testResultRole.testResultId){
-      TestResultRoles.update({_id: testResultRoleId}, {$set:{
-        status: TestResultStatus.complete,
-        resultCode: TestResultCodes.fail,
-        result: result
-      }});
+    var testResultRole = TestResultRoles.findOne({ _id: testResultRoleId });
+    if (testResultRole && testResultRole.testResultId) {
+      TestResultRoles.update({ _id: testResultRoleId }, {
+        $set: {
+          status    : TestResultStatus.complete,
+          resultCode: TestResultCodes.fail,
+          result    : result
+        }
+      });
       
       // Mark the result as a failure
       TestResults.update({
-        _id: testResultRole.testResultId,
-        status: {$ne: TestResultStatus.complete}
+        _id   : testResultRole.testResultId,
+        status: { $ne: TestResultStatus.complete }
       }, {
         $set: {
-          status: TestResultStatus.complete,
+          status    : TestResultStatus.complete,
           resultCode: TestResultCodes.fail,
-          result: result
+          result    : result
         }
       });
       
       // Abort all other roles for this test (unless they`ve already error`d out)
       TestResultRoles.update({
         testResultId: testResultRole.testResultId,
-        status: {$ne: TestResultStatus.complete},
-        _id: {$ne: testResultRoleId}
+        status      : { $ne: TestResultStatus.complete },
+        _id         : { $ne: testResultRoleId }
       }, {
         $set: {
-          abort: true,
-          status: TestResultStatus.complete,
+          abort     : true,
+          status    : TestResultStatus.complete,
           resultCode: TestResultCodes.abort,
-          result: result
+          result    : result
         }
-      }, {multi: true});
+      }, { multi: true });
     } else {
       throw new Meteor.error("unknown-test-result-role", "Failed to find testResultRole with _id " + testResultRoleId);
     }
@@ -117,17 +149,18 @@ Meteor.methods({
    * @param status
    */
   setTestResultRoleStatus(testResultRoleId, status) {
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     check(testResultRoleId, String);
     check(status, Number);
     
-    var testResultRole = TestResultRoles.findOne();
-    if(testResultRole){
-      TestResultRoles.update({_id: testResultRoleId}, {$set:{status: status}});
+    var testResultRole = TestResultRoles.findOne(),
+        testResult     = testResultRole.testResult();
+    if (testResultRole) {
+      TestResultRoles.update({ _id: testResultRoleId }, { $set: { status: status } });
       
       // update the test result status with the highest ranking status
-      if(testResultRole.testResult().status < status){
-        TestResults.update({_id: testResultRole.testResultId}, {$set: {status: status}});
+      if (testResult && testResult.status < status) {
+        TestResults.update({ _id: testResultRole.testResultId }, { $set: { status: status } });
       }
     }
   },
@@ -138,11 +171,11 @@ Meteor.methods({
    * @param status
    */
   setTestResultStepStatus(testResultStepId, status) {
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     check(testResultStepId, String);
     check(status, Number);
     
-    TestResultSteps.update({_id: testResultStepId}, {$set:{status: status}});
+    TestResultSteps.update({ _id: testResultStepId }, { $set: { status: status } });
   },
   
   /**
@@ -152,18 +185,18 @@ Meteor.methods({
    * @param resultData
    */
   setTestResultRoleResult(testResultRoleId, code, resultData) {
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     check(testResultRoleId, String);
     check(code, Number);
     
-    var testResultRole = TestResultRoles.findOne({_id: testResultRoleId});
-    TestResultRoles.update({_id: testResultRoleId}, {$set:{resultCode: code, result: resultData}});
-    if(code == TestResultCodes.fail){
-      TestResults.update({_id: testResultRole.testResultId}, {$set:{resultCode: code, result: resultData}});
-    } else if(testResultRole.testResult().resultCode == null) {
-      TestResults.update({_id: testResultRole.testResultId}, {$set:{resultCode: code}});
-    } else if(code !== TestResultCodes.pass) {
-      TestResults.update({_id: testResultRole.testResultId}, {$set:{resultCode: code}});
+    var testResultRole = TestResultRoles.findOne({ _id: testResultRoleId });
+    TestResultRoles.update({ _id: testResultRoleId }, { $set: { resultCode: code, result: resultData } });
+    if (code == TestResultCodes.fail) {
+      TestResults.update({ _id: testResultRole.testResultId }, { $set: { resultCode: code, result: resultData } });
+    } else if (testResultRole.testResult().resultCode == null) {
+      TestResults.update({ _id: testResultRole.testResultId }, { $set: { resultCode: code } });
+    } else if (code !== TestResultCodes.pass) {
+      TestResults.update({ _id: testResultRole.testResultId }, { $set: { resultCode: code } });
     }
   },
   
@@ -174,11 +207,11 @@ Meteor.methods({
    * @param resultData
    */
   setTestResultStepResult(testResultStepId, code, resultData) {
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     check(testResultStepId, String);
     check(code, Number);
     
-    TestResultSteps.update({_id: testResultStepId}, {$set:{resultCode: code, result: resultData}});
+    TestResultSteps.update({ _id: testResultStepId }, { $set: { resultCode: code, result: resultData } });
   },
   
   /**
@@ -187,11 +220,11 @@ Meteor.methods({
    * @param checks
    */
   saveTestResultStepChecks(testResultStepId, checks) {
-    check(Meteor.user(), Object);
+    check(Meteor.userId(), String);
     check(testResultStepId, String);
     
-    if(checks){
-      TestResultSteps.update({_id: testResultStepId}, {$set:{checks: checks}});
+    if (checks) {
+      TestResultSteps.update({ _id: testResultStepId }, { $set: { checks: checks } });
     }
   },
   
@@ -199,18 +232,68 @@ Meteor.methods({
    * Launch a test result run
    */
   launchTestResult(testResultId) {
-    check(Meteor.user(), Object);
+    console.info("launchTestResult: ", testResultId);
+    check(Meteor.userId(), String);
     check(testResultId, String);
-    console.info("launchTestResult: " + testResultId);
     
     var testResult = TestResults.findOne(testResultId);
     
     // validate user permissions
-    
-    if(testResult){
-      testResult.launch();
+    if (testResult) {
+      // do some quick cleanup in case this is a re-run
+      LogMessages.remove({ "context.testResultId": testResult._id });
+      Screenshots.remove({ testResultId: testResult._id });
+      
+      TestResults.update({ _id: testResult._id }, {
+        $set  : { status: TestResultStatus.launched, abort: false },
+        $unset: { resultCode: "", result: "" }
+      });
+      
+      TestResultRoles.update({ testResultId: testResult._id }, {
+        $set  : { status: TestResultStatus.staged },
+        $unset: { resultCode: "", result: "", pid: "" }
+      }, { multi: true });
+      
+      TestResultSteps.update({ testResultId: testResult._id }, {
+        $set  : { status: TestResultStatus.staged },
+        $unset: { resultCode: "", result: "", checks: "" }
+      }, { multi: true });
+      
+      // get the list of roles, create a launch token and fire away
+      TestResultRoles.find({ testResultId: testResult._id }).forEach(function (role) {
+        console.info("launchTestResult launching role: ", role._id);
+        var token   = Accounts.singleUseAuth.generate({ expires: { seconds: 30 } }),
+            command = [ ProcessLauncher.testRoleScript, "--roleId", role._id, "--token", token ].join(" "),
+            logFile = [ "test_result_role_", role._id, ".log" ].join(""),
+            proc    = ProcessLauncher.launchAutomation(command, logFile);
+        
+        TestResultRoles.update(role._id, { $set: { pid: proc.pid, status: TestResultStatus.launched } });
+        console.info("launchTestResult launched: ", role._id, " as ", proc.pid, " > ", logFile);
+      });
     } else {
       throw new Meteor.Error("invalid-testResult", "launchTestResult failed because the result could not be found", testResultId);
     }
   },
+  
+  /**
+   * Delete a test result and all of the data
+   * @param testResultId
+   */
+  deleteTestResult(testResultId){
+    console.info("deleteTestResult:", testResultId);
+    check(Meteor.userId(), String);
+    check(testResultId, String);
+    
+    // Load the record
+    var testResult = TestResults.findOne(testResultId);
+    
+    if (testResult) {
+      LogMessages.remove({ "context.testResultId": testResult._id });
+      Screenshots.remove({ testResultId: testResult._id });
+      TestResultRoles.remove({ testResultId: testResult._id });
+      TestResults.remove({ _id: testResult._id })
+    } else {
+      console.warn('deleteTestResult: test result not found for id', testResultId);
+    }
+  }
 });
--- meteor/imports/api/test_result/test_result.js
+++ meteor/imports/api/test_result/test_result.js
@@ -1,89 +1,90 @@
-import {Mongo} from 'meteor/mongo';
-import {SimpleSchema} from 'meteor/aldeed:simple-schema';
-import {SchemaHelpers} from '../schema_helpers.js';
-import {Auth} from '../auth.js';
-import {TestResultCodes} from './test_result_codes.js';
-import {TestResultStatus} from './test_result_status.js';
-
-import {TestResultRoles} from './test_result_role.js';
-import {TestResultSteps} from './test_result_step.js';
-import {Projects} from '../project/project.js';
-import {ProjectVersions} from '../project/project_version';
-import {TestCases} from '../test_case/test_case.js';
-import {TestServers} from '../test_server/test_server.js';
-import {TestRuns} from '../test_run/test_run.js';
-import {LogMessages} from '../log_message/log_message.js';
-import {Screenshots} from '../screenshot/screenshot.js';
-
-//import {ProcessLauncher} from '../process_launcher/process_launcher.js';
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { SchemaHelpers } from '../schema_helpers.js';
+import { Auth } from '../auth.js';
+import { TestResultCodes } from './test_result_codes.js';
+import { TestResultStatus } from './test_result_status.js';
+import { TestResultRoles } from './test_result_role.js';
+import { TestResultSteps } from './test_result_step.js';
+import { Projects } from '../project/project.js';
+import { ProjectVersions } from '../project/project_version';
+import { TestCases } from '../test_case/test_case.js';
+import { TestServers } from '../test_server/test_server.js';
+import { TestRuns } from '../test_run/test_run.js';
+import { LogMessages } from '../log_message/log_message.js';
+import { Screenshots } from '../screenshot/screenshot.js';
 
 /**
  * Test case result - the umbrella result for a test execution
  */
-export const TestResult = new SimpleSchema({
+export const TestResult  = new SimpleSchema({
   // Link to the project to which this test belongs
-  projectId: {
-    type: String,
+  projectId       : {
+    type      : String,
     denyUpdate: true
   },
   // Link to the project version to which this test belongs
   projectVersionId: {
-    type: String,
+    type      : String,
     denyUpdate: true
   },
   // Link to the test run
-  testRunId: {
-    type: String,
+  testRunId       : {
+    type      : String,
     denyUpdate: true,
-    optional: true
+    optional  : true
   },
   // Link to the test case via the staticId
-  testCaseId: {
-    type: String,
+  testCaseId      : {
+    type      : String,
     denyUpdate: true
   },
   // Link to the server we're running against
-  serverId: {
-    type: String,
+  serverId        : {
+    type      : String,
     denyUpdate: true
   },
   // Whether to wait for commands if there is a problem
-  pauseOnFailure: {
-    type: Boolean,
+  pauseOnFailure  : {
+    type        : Boolean,
     defaultValue: false
   },
   // The status
-  status: {
-    type: Number,
-    allowedValues: _.map(TestResultStatus, function (d) { return d; }),
-    defaultValue: TestResultStatus.staged
+  status          : {
+    type         : Number,
+    allowedValues: _.map(TestResultStatus, function (d) {
+      return d;
+    }),
+    defaultValue : TestResultStatus.staged
   },
   // Abort the test
-  abort: {
-    type: Boolean,
+  abort           : {
+    type    : Boolean,
     optional: true
   },
   // The result code
-  resultCode: {
-    type: Number,
-    allowedValues: _.map(TestResultCodes, function (d) { return d; }),
-    optional: true
+  resultCode      : {
+    type         : Number,
+    allowedValues: _.map(TestResultCodes, function (d) {
+      return d;
+    }),
+    optional     : true
   },
   // The result detail
-  result: {
-    type: Object,
+  result          : {
+    type    : Object,
     blackbox: true,
     optional: true
   },
   // Standard tracking fields
-  dateCreated: {
-    type: Date,
-    autoValue: SchemaHelpers.autoValueDateCreated,
+  dateCreated     : {
+    type      : Date,
+    autoValue : SchemaHelpers.autoValueDateCreated,
     denyUpdate: true
   },
-  createdBy: {
-    type: String,
-    autoValue: SchemaHelpers.autoValueCreatedBy,
+  createdBy       : {
+    type      : String,
+    autoValue : SchemaHelpers.autoValueCreatedBy,
     denyUpdate: true
   }
 });
@@ -97,22 +98,22 @@ TestResults.allow(Auth.ruleSets.allow.ifAuthenticated);
  */
 TestResults.helpers({
   roleResults() {
-    return TestResultRoles.find({testResultId: this._id});
+    return TestResultRoles.find({ testResultId: this._id });
   },
   project() {
-    return Projects.findOne({_id: this.projectId});
+    return Projects.findOne({ _id: this.projectId });
   },
   projectVersion() {
-    return ProjectVersions.findOne({_id: this.projectVersionId});
+    return ProjectVersions.findOne({ _id: this.projectVersionId });
   },
   testCase() {
-    return TestCases.findOne({staticId: this.testCaseId, projectVersionId: this.projectVersionId});
+    return TestCases.findOne({ staticId: this.testCaseId, projectVersionId: this.projectVersionId });
   },
   server() {
-    return TestServers.findOne({staticId: this.serverId, projectVersionId: this.projectVersionId});
+    return TestServers.findOne({ staticId: this.serverId, projectVersionId: this.projectVersionId });
   },
   testRun() {
-    return TestRuns.findOne({staticId: this.testRunId, projectVersionId: this.projectVersionId});
+    return TestRuns.findOne({ staticId: this.testRunId, projectVersionId: this.projectVersionId });
   },
   isStaged() {
     return this.status = TestResultStatus.staged
@@ -128,32 +129,5 @@ TestResults.helpers({
       TestResultStatus.complete,
       TestResultStatus.skipped
     ], this.status)
-  },
-  /**
-   * Launch a staged test result
-   */
-  launch() {
-    console.debug("TestResult.launch: " + this._id);
-    if(!Meteor.isServer) throw new Meteor.Error("server-only", "This method can only be executed on the server");
-
-    // do some quick cleanup in case this is a re-run
-    var result = this;
-    LogMessages.remove({"context.testResultId": result._id});
-    Screenshots.remove({testResultId: result._id});
-    TestResults.update({_id: result._id}, {$set: {status: TestResultStatus.launched, abort: false}, $unset: {resultCode: "", result: ""}});
-    TestResultRoles.update({testResultId: result._id}, {$set: {status: TestResultStatus.staged}, $unset: {resultCode: "", result: "", pid: ""}}, {multi: true});
-    TestResultSteps.update({testResultId: result._id}, {$set: {status: TestResultStatus.staged}, $unset: {resultCode: "", result: "", checks: ""}}, {multi: true});
-
-    // get the list of roles, create a launch token and fire away
-    TestResultRoles.find({testResultId: result._id}).forEach(function (role) {
-      console.info("launchTestResult launching role: " + role._id);
-      var token = Accounts.singleUseAuth.generate({ expires: { seconds: 5 } }),
-        command = [ProcessLauncher.testRoleScript, "--roleId", role._id, "--token", token].join(" "),
-        logFile = ["test_result_role_", role._id, ".log"].join(""),
-        proc = ProcessLauncher.launchAutomation(command, logFile);
-
-      TestResultRoles.update(role._id, {$set: {pid: proc.pid, status: TestResultStatus.launched}});
-      console.info("launchTestResult launched: " + role._id + " as " + proc.pid + " > " + logFile);
-    });
   }
 });
