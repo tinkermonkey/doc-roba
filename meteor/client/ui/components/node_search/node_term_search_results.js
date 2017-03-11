a6700d24246ceab77afc5659f9be429510143e6b
austinsand <austinsand@gmail.com>
2017-03-11 07:06:45 -0500

    Code Formatting

--- meteor/client/ui/components/node_search/node_term_search_results.js
+++ meteor/client/ui/components/node_search/node_term_search_results.js
@@ -1,8 +1,6 @@
 import './node_term_search_results.html';
 import './node_search.css';
-
-import {Template} from 'meteor/templating';
-
+import { Template } from 'meteor/templating';
 import '../svg_snippets/standalone_node_snippet.js';
 
 /**
@@ -17,7 +15,7 @@ import '../svg_snippets/standalone_node_snippet.js';
  */
 Template.NodeTermSearchResults.helpers({
   highlightTerm(param, term) {
-    if(param && term){
+    if (param && term) {
       return param.replace(new RegExp('(' + term + ')', 'i'), '<span class="search-highlight">$1</span>');
     }
     
@@ -34,19 +32,19 @@ Template.NodeTermSearchResults.events({});
  * Template Created
  */
 Template.NodeTermSearchResults.created = function () {
-
+  
 };
 
 /**
  * Template Rendered
  */
 Template.NodeTermSearchResults.rendered = function () {
-
+  
 };
 
 /**
  * Template Destroyed
  */
 Template.NodeTermSearchResults.destroyed = function () {
-
+  
 };
