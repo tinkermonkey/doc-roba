42b49e6809fa1c051992c304b8a9950556614bff
austinsand <austinsand@gmail.com>
2017-03-26 13:14:04 -0400

    Added display of Custom test steps

--- meteor/client/ui/pages/test_result/test_result_steps/test_result_step_custom.js
+++ meteor/client/ui/pages/test_result/test_result_steps/test_result_step_custom.js
@@ -1,5 +1,6 @@
 import './test_result_step_custom.html';
 import { Template } from 'meteor/templating';
+let Prism = require('prismjs');
 
 /**
  * Template Helpers
@@ -22,7 +23,22 @@ Template.TestResultStepCustom.onCreated(() => {
  * Template Rendered
  */
 Template.TestResultStepCustom.onRendered(() => {
+  let instance = Template.instance();
   
+  // Watch for data changes and re-render
+  instance.autorun(function () {
+    let data    = Template.currentData(),
+        context = data.logContextMessage();
+
+    if (context.data && context.data.length) {
+      let stepData = context.data[0].data;
+      if(stepData.data && stepData.data.code){
+        instance.$('.code').html(Prism.highlight(stepData.data.code, Prism.languages.javascript));
+      } else {
+        console.error("TestResultStepCustom didn't find code in the step context:", context);
+      }
+    }
+  });
 });
 
 /**
