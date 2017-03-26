8b67181a5778f03c91efbe19e58b2b2f962008bc
austinsand <austinsand@gmail.com>
2017-03-26 12:49:52 -0400

    Issue #44 - Fixed word wrap in the Node Snippet

--- meteor/client/ui/components/svg_snippets/node_snippet.js
+++ meteor/client/ui/components/svg_snippets/node_snippet.js
@@ -17,12 +17,6 @@ Template.NodeSnippet.helpers({
       default:
         return "node-page"
     }
-  },
-  titleX() {
-    return DocTreeConfig.nodes.width / 2;
-  },
-  titleY() {
-    return DocTreeConfig.nodes.titleHeight + DocTreeConfig.nodes.borderWidth;
   }
 });
 
@@ -45,15 +39,22 @@ Template.NodeSnippet.onRendered(() => {
   let instance = Template.instance();
   
   instance.autorun(() => {
-    let data = Template.currentData();
+    let data           = Template.currentData(),
+        titleContainer = d3.select("#" + instance.elementId).select(".node-title-container");
     
-    setTimeout(() => {
-      // TODO: This is breaking the Blaze DOM management, it's throwing errors creating/tearing down the elements
-      d3.select("#" + instance.elementId)
-          .selectAll("text.node-title")
-          //.call(Util.wrapSvgText, DocTreeConfig.nodes.width - 2 * DocTreeConfig.nodes.borderWidth)
+    if(data && data.title){
+      // Remove any existing node title
+      titleContainer.select('text').remove();
+  
+      // Set the node title
+      titleContainer.append('text')
+          .attr('class', 'node-title pre-render no-select-complete')
+          .attr('x', DocTreeConfig.nodes.width / 2)
+          .attr('y', DocTreeConfig.nodes.titleHeight + DocTreeConfig.nodes.borderWidth)
+          .text(data.title)
+          .call(Util.wrapSvgText, DocTreeConfig.nodes.width - 2 * DocTreeConfig.nodes.borderWidth)
           .classed('pre-render', false);
-    }, 30);
+    }
   });
 });
 
