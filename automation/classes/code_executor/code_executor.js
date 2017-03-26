01bc314ca3b697cad206d8505468b8fbcf7f401d
austinsand <austinsand@gmail.com>
2017-03-26 12:58:02 -0400

    Updated CodeExecutor to user the LogAssistant

--- automation/classes/code_executor/code_executor.js
+++ automation/classes/code_executor/code_executor.js
@@ -1,10 +1,6 @@
-"use strict"
+"use strict";
 
-var log4js    = require('log4js'),
-    logger    = log4js.getLogger('project'),
-    advLogger = log4js.getLogger('adventure');
-
-logger.setLevel('TRACE');
+let logger = require('../log_assistant.js').getLogger();
 
 class CodeExecutor {
   /**
@@ -12,7 +8,7 @@ class CodeExecutor {
    * @param code
    */
   constructor (code) {
-    advLogger.debug("Creating code executor:", code);
+    logger.debug("Creating code executor:", code);
     this.code      = code || '';
     this.variables = [];
   }
@@ -21,8 +17,8 @@ class CodeExecutor {
    * Execute the code
    */
   execute () {
-    advLogger.debug("CodeExecutor executing:", this.code);
-    var executor  = this,
+    logger.debug("CodeExecutor executing:", this.code);
+    let executor  = this,
         setupCode = 'var ',
         startTime = Date.now(),
         error, result;
@@ -34,16 +30,16 @@ class CodeExecutor {
     
     // Evaluate the code
     try {
-      advLogger.debug("Executing code:", setupCode + executor.code);
+      logger.debug("Executing code:", setupCode + executor.code);
       result = eval(setupCode + executor.code);
     } catch (e) {
-      advLogger.error("Code execution failed:", e.toString(), e.stack);
+      logger.error("Code execution failed:", e.toString(), e.stack);
       error = e;
     }
     
     // Return the result
     executor.executionTime = Date.now() - startTime;
-    advLogger.debug("Code executed in [" + executor.executionTime + "] ms:", result);
+    logger.debug("Code executed in [" + executor.executionTime + "] ms:", result);
     return {
       error : error,
       result: result
@@ -57,7 +53,7 @@ class CodeExecutor {
    * @param defaultValue
    */
   addVariable (name, value, defaultValue) {
-    advLogger.debug("CodeExecutor adding variable:", name);
+    logger.debug("CodeExecutor adding variable:", name);
     this.variables.push({ name: name, value: value, defaultValue: defaultValue });
   }
 }
